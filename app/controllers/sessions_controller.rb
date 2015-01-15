99f7524f611d9d39944692368b4aa259005e22d2
AlexMasonAM <alexmmason@gmail.com>
2015-01-14 17:54:11 -0800

    authenticated Users, added logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-  
+    u = User.find_by(username: params[:user][:password])
+
+    if u && u.authenticate(params[:user][:password])
+      session[:user_id] = u.id.to_s # guarentees that the user id session will always be a string
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    redirect_to login_path
   end
 end
